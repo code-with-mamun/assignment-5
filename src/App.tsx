6dc0ae346c4169489338527f618e7301c5b43d63
MA MU N <mamunlevel225522@gmail.com>
2026-09-13 22:31:03 +0600

    i have pushed the footer section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import Nav from "./components/Nav";
 import Hero from "./components/Hero";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <>
       <Nav></Nav>
       <Hero></Hero>
+      <Footer></Footer>
     </>
   );
 }
