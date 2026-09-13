035b328c83898d3fee78fbc370e23cfbca4d4675
MA MU N <mamunlevel225522@gmail.com>
2026-09-13 23:19:42 +0600

    i pushed the technology section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import Nav from "./components/Nav";
+import { Suspense } from "react";
 import Hero from "./components/Hero";
+import Nav from "./components/Nav";
+
+import type { ITechnologyType } from "./types/type";
+import Technology from "./technologys/Technology";
 import Footer from "./components/Footer";
+import { ToastContainer } from "react-toastify";
 
 function App() {
+  const technologiesFetch = async () => {
+    const response = await fetch("/technology.json");
+    const data = await response.json();
+    return data;
+  };
+
+  const technologyPromise: Promise<ITechnologyType[]> = technologiesFetch();
+  // console.log(technologyPromise);
+
   return (
     <>
       <Nav></Nav>
       <Hero></Hero>
+
+      <Suspense fallback={<h2 className="text-center"> Loading ......</h2>}>
+        <Technology technologyPromise={technologyPromise}></Technology>
+      </Suspense>
+      <ToastContainer></ToastContainer>
+
       <Footer></Footer>
     </>
   );
---
+++ src/types/type.ts
@@ -0,0 +1,12 @@
+export interface ITechnologyType {
+     id: string,
+    name: string,
+    category: string,
+    description: string,
+    icon: string,
+    rating: number,
+    difficulty: string,
+    badge: string,
+    color: string,
+    bgColor: string
+}
